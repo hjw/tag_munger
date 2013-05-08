9d7edb15383659da3f82ada0e852b2c42a4fe5fb
Hawley Waldman <hawleyw@gmail.com>
2013-05-07 22:26:48 -0400

    Browse, changing album data and dryrun/write-data done.
    Basic functionality needed for Craig to do custom munging of
    the tape library.

--- bin/tag_utility.rb
+++ bin/tag_utility.rb
@@ -1,25 +1,30 @@
 #!/usr/bin/env ruby
 
+############################
+# tag_utility.rb
+# A small utility which will let you display album and track 
+# tag information from mp3 files and will allow you to apply
+# our album name generator for mp3 files in the "tape library"
+# 
+
 #$LOAD_PATH.push File.expand_path("../../lib",__FILE__)
 require 'optparse'
-require_relative '../lib/futest'
-require_relative '../lib/tag_munger'
+require_relative '../lib/tag_munger' #the class that handles our munging
 
 VERSION="version 0.0.1"
 
 options = {}
 # defaults
 options[:directory] = "."
+options[:dry_run] = true
 
 
 OptionParser.new do |opts|
-  opts.banner = "Usage: tag_utility.rb [options]"
+  opts.banner = "Usage: tag_utility.rb [options] ***Defaults to dry_run/report mode***\n\t\tSpecify --write-changes option to actually change tags"
   opts.version = VERSION
 
   opts.on("-b [FLAGS]","--browse [FLAGS]",\
           "\n\t\tDisplay the current mp3 metadata.\n\t\tTakes optional, comma separated list of tags you're interested in (album,track)") do |flags|
-    #options.inplace = true
-    #options.extension = flags || ''
     options[:browse] = flags || true
   end
 
@@ -27,22 +32,27 @@ OptionParser.new do |opts|
     options[:directory] = library_root
   end
 
-  opts.on("--albums", "Deal with album info") do |a|
+  opts.on("-a","--albums", "Derive new album tag for mp3 files") do |a|
+    # runs the generic album name generator. Does a dry run by default
     options[:albums] = a
+  end
+
+  opts.on("--write-changes", "Changes the tags in the files (takes you out of dry run mode)") do |w|
+    options[:dry_run] = false
   end
   
 end.parse!
 
-Futest.greet
-p options
-p ARGV
+tagger = TagMunger.new(options[:directory], options[:dry_run])
 
-tagger = TagMunger.new(options[:directory])
-if options[:browse].class == String
-  which_tags =  options[:browse].split(",")
+if options.has_key?(:browse)
+  if options[:browse].class == String
+    which_tags =  options[:browse].split(",")
+  end
+  tagger.browseLibrary(which_tags)
 end
 
-p "which_tags: #{which_tags}"
-
-tagger.browseLibrary(which_tags)
+if options.has_key?(:albums)
+  tagger.fix_album_tags
+end
 
--- lib/tag_munger.rb
+++ lib/tag_munger.rb
@@ -5,8 +5,10 @@ require 'taglib'
 require 'pp'
 
 class TagMunger
-  def initialize(library_root)
+  
+  def initialize(library_root=".", dry_run=true)
     @library_root = library_root
+    @dry_run = dry_run
   end
 
   def browseLibrary(which_tags)
@@ -39,6 +41,59 @@ class TagMunger
         puts "uh-oh. There was a problem opening #{name}"
       end
     end
+  end
+  #########################################
+  # def fix_album_tags
+  # Global fixing of album tags.
+  # dummy_name = "D01_1910_Disc_English_10d.mp3"
+  #
+  # Set all mp3 files that start with D## to have an 
+  # album tag of D##_courseType.  
+  # 
+  # The course type is scraped from the file name.
+  #
+  # ie: makes album tag "D01_10d" for a file named D01_1910_Disc_English_10d.mp3 
+  def fix_album_tags
+    file_list = select_files("#{@library_root}/**/D[0-9][0-9]_*.mp3")
 
+    album_name = ""
+    temp_hash = {}
+
+    # create album_name from file name 
+    # "D09_0855_GS_10d.mp3" gets album "D09_10d"
+    file_list.each do |name|
+      w = (File.basename(name)).split("_")
+      album_name = [ w[0],  w[-1].chomp(".mp3")].join("_")
+      temp_hash[name] = album_name
+    end
+    puts "New album data prepared, now hold on while I write it into the #{file_list.count} mp3 files."
+
+    if !@dry_run then
+      temp_hash.each do |file_name, album_tag|
+        ok = TagLib::FileRef.open(file_name) do |f|
+          tag = f.tag
+          tag.album = album_tag
+          f.save
+        end
+        if ok == false then
+          puts "uh-oh.  There was a problem saving / openning #{file_name}"
+        end
+      end
+    else #@dry_run == true
+      pp temp_hash.sort
+    end
+    puts "finished the global album name fix."
+  end #end.fix_album_tags
+  private
+
+  def select_files(match_string)
+    Dir.glob(match_string) do |name|
+      file_list << name
+    end
+    if file_list.length == 0
+      pp "empty file list from root: #{@library_root}"
+    else
+      pp "file_list is #{file_list.count} long."
+    end
   end
 end
