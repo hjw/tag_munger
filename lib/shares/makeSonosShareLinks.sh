4e0d0e7370070508118744739deca6d347fcc364
Craig D. Miller <cmiller@srri.umass.edu>
2014-10-09 09:38:03 -0400

    Sonos Share Links : add missing English-only audio discourses to Khmer and Burmese STP shares.  Required because day 9 discourse is audio-only.

--- lib/shares/makeSonosShareLinks.sh
+++ lib/shares/makeSonosShareLinks.sh
@@ -206,6 +206,7 @@ then
 	cd $SHAREDIR; echo making links in $SHAREDIR
 	ln -sfn  $AUDIOBASE/'STP Course Sets/STP Burmese/STP E-Burmese Instructions'
 	ln -sfn  $AUDIOBASE/'STP Course Sets/STP Burmese/STP Burmese Discourses'
+	ln -sfn  $AUDIOBASE/'STP Course Sets/STP English-only/STP English Discourses'
 	ln -sfn  $AUDIOBASE/'Special Chantings/Satipatthana_Sutta_Jan_1985_wSM.mp3'
 	#ln -sfn  $AUDIOBASE/'Dhamma Service/Benefits of Dhamma Service/D10_2030_Burmese_Benefits_of_D.Service.mp3'  # does not exist in Burmese
 else
@@ -219,6 +220,7 @@ then
 	cd $SHAREDIR; echo making links in $SHAREDIR
 	ln -sfn  $AUDIOBASE/'STP Course Sets/STP Khmer/STP E-Khmer Instructions'
 	ln -sfn  $AUDIOBASE/'STP Course Sets/STP Khmer/STP Khmer Discourses'
+	ln -sfn  $AUDIOBASE/'STP Course Sets/STP English-only/STP English Discourses'
 	ln -sfn  $AUDIOBASE/'Special Chantings/Satipatthana_Sutta_Jan_1985_wSM.mp3'
 else
 	echo "$FATAL folder $SHAREDIR does not exist"; exit 100
