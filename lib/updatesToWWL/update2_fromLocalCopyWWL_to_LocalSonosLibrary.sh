fda36a984c9a048e3793d209817df821a862c9b5
Craig D. Miller <cmiller@srri.umass.edu>
2017-12-29 13:54:28 -0500

    clean up share instructions, update WWL scripts for 2017

--- lib/updatesToWWL/update2_fromLocalCopyWWL_to_LocalSonosLibrary.sh
+++ lib/updatesToWWL/update2_fromLocalCopyWWL_to_LocalSonosLibrary.sh
@@ -1,13 +1,14 @@
 # make a local copy called SonosLibrary on DharaWWL silver 1gb portable HD, making slight rearrangements
-WWL_FOLDER='WWD_2015'
+WWL_FOLDER='WWL_2017'
 
 mkdir -v /Volumes/DharaWWL/SonosLibrary
 mkdir -v /Volumes/DharaWWL/SonosLibrary/Sonos\ Customizations/
 mkdir -v /Volumes/DharaWWL/SonosLibrary/Sonos\ Customizations/Sonos\ Share\ Info
 
-rsync -avz /Volumes/DharaWWL/$WWL_FOLDER/WWL_HDD/* /Volumes/DharaWWL/SonosLibrary
-rsync -avz /Volumes/DharaWWL/$WWL_FOLDER/WWL_Exceptions/* /Volumes/DharaWWL/SonosLibrary/Additional\ Material
 rsync -avz /Volumes/DharaWWL/VMC_Additional_Files/VMC\ Alarms /Volumes/DharaWWL/SonosLibrary/Sonos\ Customizations/
 rsync -avz /Volumes/DharaWWL/tag_munger/lib/shares/* /Volumes/DharaWWL/SonosLibrary/Sonos\ Customizations/Sonos\ Share\ Info
+rsync -avz /Volumes/DharaWWL/$WWL_FOLDER/WWL_mp3/* /Volumes/DharaWWL/SonosLibrary
+#rsync -avz /Volumes/DharaWWL/$WWL_FOLDER/WWL_Exceptions/* /Volumes/DharaWWL/SonosLibrary/Additional\ Material
+
 echo '---'
 echo 'now run the taggging command, then updateToNAS.sh'
